f14dee570b96c2a2fc2afa7e5a455fd4adcaa808
Massimiliano <massimiliano.casini@gmail.com>
2021-06-06 09:35:50 +0200

    Delete arduino_secrets.h

--- firmware/prodinoWIFI/arduino_secrets.h
+++
@@ -1,9 +0,0 @@
-// WiFi settings
-#define SSID				"MisiSpot"
-#define SSID_PASSWORD		"multiactivity"
-
-// MQTT server settings. 
-#define MQTT_SERVER         "cm3home" // Change it with yours data.
-#define MQTT_PORT           1883 // Change it with yours data.
-#define MQTT_USER           "" // Change it with yours data.
-#define MQTT_PASS           "" // Change it with yours data.
