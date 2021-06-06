511aa1ebba8406033525286974804c70332738b1
Massimiliano Casini <massimiliano.casini@gmail.com>
2021-06-06 09:36:35 +0200

    Flussometro

--- firmware/prodinoWIFI/arduino_secrets.h
+++ firmware/prodinoWIFI/arduino_secrets.h
@@ -1,7 +1,6 @@
 // WiFi settings
 #define SSID				"MisiSpot"
 #define SSID_PASSWORD		"multiactivity"
-
 // MQTT server settings. 
 #define MQTT_SERVER         "cm3home" // Change it with yours data.
 #define MQTT_PORT           1883 // Change it with yours data.
